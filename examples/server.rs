e64cb7e31741ae4407b9804d1d02651b1f023673
Y. T. Chung <zonyitoo@gmail.com>
2017-08-12 13:37:41 +0800

    Add bench clients and configurable example

--- examples/server.rs
+++ examples/server.rs
@@ -12,7 +12,50 @@ use futures::stream::Stream;
 use tokio_core::reactor::Core;
 use tokio_io::AsyncRead;
 use tokio_io::io::copy;
-use tokio_kcp::KcpListener;
+use tokio_kcp::{KcpConfig, KcpListener, KcpNoDelayConfig};
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+enum TestMode {
+    Default,
+    Normal,
+    Fast,
+}
+
+fn get_config(mode: TestMode) -> KcpConfig {
+    let mut config = KcpConfig::default();
+    config.wnd_size = Some((128, 128));
+    match mode {
+        TestMode::Default => {
+            config.nodelay = Some(KcpNoDelayConfig {
+                                      nodelay: false,
+                                      interval: 10,
+                                      resend: 0,
+                                      nc: false,
+                                  });
+        }
+        TestMode::Normal => {
+            config.nodelay = Some(KcpNoDelayConfig {
+                                      nodelay: false,
+                                      interval: 10,
+                                      resend: 0,
+                                      nc: true,
+                                  });
+        }
+        TestMode::Fast => {
+            config.nodelay = Some(KcpNoDelayConfig {
+                                      nodelay: true,
+                                      interval: 10,
+                                      resend: 2,
+                                      nc: true,
+                                  });
+
+            config.rx_minrto = Some(10);
+            config.fast_resend = Some(1);
+        }
+    }
+
+    config
+}
 
 fn main() {
     let _ = env_logger::init();
@@ -22,9 +65,19 @@ fn main() {
         .unwrap_or_else(|| "127.0.0.1:2233".to_string());
     let addr = addr.parse::<SocketAddr>().unwrap();
 
+    let mode = env::args().nth(2).unwrap_or_else(|| "default".to_string());
+    let mode = match &mode[..] {
+        "default" => TestMode::Default,
+        "normal" => TestMode::Normal,
+        "fast" => TestMode::Fast,
+        _ => panic!("Unrecognized mode {}", mode),
+    };
+
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-    let listener = KcpListener::bind(&addr, &handle).unwrap();
+
+    let config = get_config(mode);
+    let listener = KcpListener::bind_with_config(&addr, &handle, config).unwrap();
     println!("listening on: {}", addr);
 
     let echo = listener.incoming().for_each(|(stream, addr)| {

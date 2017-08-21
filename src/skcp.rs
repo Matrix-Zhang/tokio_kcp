b8669f7541e62aa5945d49b2431982263786bd2b
Y. T. Chung <zonyitoo@gmail.com>
2017-08-22 03:24:21 +0800

    Check send buffer size only if waiting for allocating conv

--- src/skcp.rs
+++ src/skcp.rs
@@ -339,7 +339,7 @@ impl SharedKcp {
             return Err(From::from(io::Error::new(ErrorKind::WouldBlock, "too many pending packets")));
         }
 
-        if !inner.sent_first {
+        if !inner.sent_first && inner.kcp.waiting_conv() {
             assert!(buf.len() <= inner.kcp.mss() as usize, "First packet must be smaller than mss");
         }
 
